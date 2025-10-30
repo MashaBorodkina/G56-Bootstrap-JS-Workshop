82fcb6552b207b03c9e98db0be8a215c7940dd3e
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-10-30 16:59:17 +0300

    feat(todo-list): improve code structure by adding comments for clarity

--- script.js
+++ script.js
@@ -22,18 +22,23 @@ const pushButton = document.getElementById("add-todo");
 pushButton.addEventListener("click", addTodo);
 
 function addTodo(event) {
-  event.preventDefault();
+  event.preventDefault(); // prevent restarting the page
 
   const cloneElement = document.getElementById("list-element").cloneNode(true);
   cloneElement.removeAttribute("id");
   cloneElement.classList.remove("d-none");
 
+  // get form values
+
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
   const due = document.getElementById("due-date").value;
+
   const select = document.getElementById("assign");
   const assignedToSelect =
     document.getElementById("assign").options[select.selectedIndex].text;
+
+  // add values to cloned element
 
   cloneElement.querySelector(".title-list-element").textContent = title;
   cloneElement.querySelector(".description-list-element p").textContent =
@@ -44,6 +49,8 @@ function addTodo(event) {
 
   const listContainer = document.getElementById("todo-list-container");
   listContainer.appendChild(cloneElement);
+
+  // reset form fields
 
   document.getElementById("form-container").reset();
 }
